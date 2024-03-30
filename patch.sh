1eca5af420edabb7bd8c3e7b7add626c134a7f7f
AAGaming <aagaming@riseup.net>
2024-03-30 01:35:38 -0400

    disable broken selinux patch

--- patch.sh
+++ patch.sh
@@ -4,7 +4,7 @@ set -eux
 # Apply patches.
 git apply patches/incremental_delivery.patch --whitespace=fix
 git apply patches/libpng.patch --whitespace=fix
-git apply patches/selinux.patch  --whitespace=fix
+# git apply patches/selinux.patch  --whitespace=fix
 git apply patches/protobuf.patch --whitespace=fix
 git apply patches/aapt2-3.patch --whitespace=fix
 git apply patches/androidfw-2.patch --whitespace=fix
